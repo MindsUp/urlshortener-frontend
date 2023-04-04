3f57742a26604cde84dcf1e6dab5558fb0c1b507
Nilson Aguiar <c_naguiar@ojolabs.com>
2023-04-04 21:49:13 +0200

    Initial structure

---
+++ src/components/header/Header.svelte
@@ -0,0 +1,11 @@
+<header>
+
+  <img src="/logo.png" alt="Logo" />
+  <nav>
+    <ul>
+      <li><a href="/">Home</a></li>
+      <li><a href="/about-us">About Us</a></li>
+    </ul>
+  </nav>
+
+</header>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+  import Header from '../components/header/Header.svelte';
+</script>
+
+<div class="app">
+
+  <Header />
+
+  <main>
+    <slot />
+  </main>
+
+  <footer>
+  </footer>
+
+</div>
+
