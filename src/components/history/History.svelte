5d089eab826ca388d324fe8776b27d117e00f880
Breno Arantes <breno.arantes97@gmail.com>
2023-05-07 16:48:56 +0200

    #7: Update the style to use d-flexx intead of row/col
    - Truncate the url to fit the box

--- src/components/history/History.svelte
+++ src/components/history/History.svelte
@@ -11,7 +11,7 @@
   onMount(async () => {
     loadData()
       .then((data) => {
-        history = data.resource.slice(0, 10);
+        history = data.resource.slice(0, 5);
       })
       .catch((error) => {
         console.log(error);
@@ -27,29 +27,19 @@
         <h5 class="card-title">History</h5>
         <ol class="list-group list-group-flush list-group-numbered">
           {#each history as shortener}
-            <li class="list-group-item d-flex justify-content-evenly">
-              <div class="row">
-                <div class="col-10">
-                  <div class="row">
-                    <div class="col">
-                      <div class="fw-bold">{shortener.alias}</div>
-                    </div>
-                  </div>
-                  <div class="row">
-                    <div class="col">
-                      <span
-                        ><a href="{shortener.url}" target="_blank"
-                          >{shortener.url.substring(0, 32)}...</a
-                        ></span
-                      >
-                    </div>
-                  </div>
+            <li class="list-group-item d-flex justify-content-between align-items-start">
+              <div class="ms-2 me-auto text-truncate">
+                <div class="align-self-start">
+                  <div class="fw-bold">{shortener.alias}</div>
+                </div>
+                <div class="align-self-center col text-truncate">
+                  <span><a href="{shortener.url}" target="_blank">{shortener.url}</a></span>
+                </div>
+                <div class="align-self-end">
                   <span>Created at: <Time relative timestamp="{shortener.createdAt}" /></span>
                 </div>
-                <div class="col-2 text-center">
-                  <button type="button" class="btn btn-outline-danger"><Trash /></button>
-                </div>
               </div>
+              <button type="button" class="btn btn-outline-danger"><Trash /></button>
             </li>
           {/each}
         </ol>
